d6833bdc23c7fe4c3d39ca5836264e6ec6c57025
antonioanderson1 <andersonjuliao1230@gmail.com>
2023-11-08 09:53:57 -0300

    Ktde de palavras no jogo

--- jogo.py
+++ jogo.py
@@ -9,3 +9,10 @@ print(' '+("="*3)+(" Design de Software ") +("="*3))
 print('\n')
 print('Comandos: desisto') ##########################
 
+while True:
+    ktde = int(input("Kuantidade de palavras kue você deseja jogar: "))
+    lista_jogo = funcoes.filtra(palavras.PALAVRAS,ktde)
+    if lista_jogo == []:
+        print("Kuantidade Inválida! Tente uma Kuantidade menor de letras!")
+    else:
+        break
