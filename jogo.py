f092ee2aaac0d1c50223e1ec88fb944cd65c5fe8
antonioanderson1 <andersonjuliao1230@gmail.com>
2023-11-08 08:55:24 -0300

    estrutura jogo

---
+++ jogo.py
@@ -0,0 +1,10 @@
+import palavras
+import funcoes
+
+print(' '+("="*27))
+print("|" + (' ')*27 +'|')
+print("|" + " Bem-vindo ao Insper Termo "+"|")
+print("|" + (' ')*27 +'|')
+print(' '+("="*3)+(" Design de Software ") +("="*3))
+print('\n')
+print('Comandos: desisto') ##########################
