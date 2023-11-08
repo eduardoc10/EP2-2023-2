1a9632012e9dece20e7ef77f88aff7c6d52c259c
Eduardo Candeias <122449805+eduardoc10@users.noreply.github.com>
2023-11-08 09:26:46 -0300

    oi

--- jogo.py
+++ jogo.py
@@ -7,15 +7,4 @@ print("|" + " Bem-vindo ao Insper Termo "+"|")
 print("|" + (' ')*27 +'|')
 print(' '+("="*3)+(" Design de Software ") +("="*3))
 print('\n')
-
-while True:
-    qtde = int(input("Deseja jogar com uma palavra de quantas letras?"))
-    lista_jogo = funcoes.filtra(palavras.PALAVRAS, qtde)
-    if lista_jogo == []:
-        print("Quantidade inválida! Tente um número menor de letras")
-    else:
-        break
-
-print(lista_jogo)
 print('Comandos: desisto') ##########################
-
