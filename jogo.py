b0d246614d8bc187fee2707634b6dcd4d158ef6e
antonioanderson1 <andersonjuliao1230@gmail.com>
2023-11-11 00:40:05 -0300

    adição de cores

--- jogo.py
+++ jogo.py
@@ -1,6 +1,13 @@
 import palavras
 import funcoes
 
+COR_AZUL = '\033[94m'
+COR_AMARELA = '\033[93m'
+COR_CINZA = '\033[90m'
+COR_RESET = '\033[0m'
+COR_VERMELHA = '\033[91m'
+COR_VERDE = '\033[92m'
+
 print(' '+("="*27))
 print("|" + (' ')*27 +'|')
 print("|" + " Bem-vindo ao Insper Termo "+"|")
