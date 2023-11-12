d3cdca67c4687cc8ac6fc0b50a4c2584b406fd7e
antonioanderson1 <andersonjuliao1230@gmail.com>
2023-11-12 20:40:13 -0300

    Interação usuário com o jogo

--- jogo.py
+++ jogo.py
@@ -39,6 +39,45 @@ print(palavra_sorteada)
 
 print('\n'+'Já tenho uma palavra! Tente adivinhá-la!')
 
+tentativas = ['desisto']
+matriz_letras = []
+i = qtde+1
+quit = ''
+while i>0:
+    palavra_especulada_geral = input('\n'+'Você tem '+str(i)+' tentativa(s)'+'\n'+'Qual é o seu palpite? ')
+    palavra_especulada = palavra_especulada_geral.lower()
+    if palavra_especulada not in tentativas:
+        tentativas.append(palavra_especulada)
+        posicao_letras = funcoes.inidica_posicao(palavra_sorteada,palavra_especulada)
+        if posicao_letras == [0]*len(palavra_sorteada):
+            parabens = COR_AMARELA + 'PARABÉNS!! Você ganhou!!!'+COR_RESET
+            print('\n'+'{0}'.format(parabens))
+            break
+        elif posicao_letras == []:
+            quantidade_letras = COR_VERMELHA + str(qtde) + COR_RESET
+            print('\n'+"Palavra Inválida. Tente uma palavra de "+'{0}'.format(quantidade_letras)+" letras")
+        else:
+            j = 0
+            mostra_usuario = "| "
+            for letra in palavra_especulada:
+                if posicao_letras[j] == 0:
+                    letra = COR_AZUL + letra + COR_RESET
+                    j+=1
+                elif posicao_letras[j] == 1:
+                    letra = COR_AMARELA + letra + COR_RESET
+                    j+=1
+                elif posicao_letras[j] == 2:
+                    letra = COR_CINZA + letra + COR_RESET
+                    j+=1
+                mostra_usuario = mostra_usuario+ letra + " | "
+            matriz_letras.append(mostra_usuario)
+            print('\n')
+            for palavra in matriz_letras:
+                print((' '+'---')*len(palavra_especulada))                    
+                print(palavra)
+                print((' '+'---')*len(palavra_especulada))
+            i-=1
+
 def print_word_not_tried():
     print('\n'+ "Você já tentou essa palavra. Tente uma nova!"+'\n')
 
