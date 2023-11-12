46705405fb3f5c2d304b30c4281c18a13fa361dc
antonioanderson1 <andersonjuliao1230@gmail.com>
2023-11-12 18:39:13 -0300

    Correção de funções

--- jogo.py
+++ jogo.py
@@ -8,17 +8,21 @@ COR_RESET = '\033[0m'
 COR_VERMELHA = '\033[91m'
 COR_VERDE = '\033[92m'
 
-def print_welcome_message():
-    print((' '+("="*27))+"\n"+("|" + (' ')*27 +'|')+"\n"+("|" + " Bem-vindo ao Insper Termo "+"|")+"\n"+("|" + (' ')*27 +'|')+"\n"+(' '+("="*3)+(" Design de Software ") +("="*3)))
-    print('\n'+'Comandos: Para sair do jogo digite "desisto"'+'\n')
+print((' '+("="*27))+"\n"+("|" + (' ')*27 +'|')+"\n"+("|" + " Bem-vindo ao Insper Termo "+"|")+"\n"+("|" + (' ')*27 +'|')+"\n"+(' '+("="*3)+(" Design de Software ") +("="*3)))
+print('\n'+'Comandos: Para sair do jogo digite "desisto"'+'\n')
 
-def print_rules(qtde_letras, tentativas, azul, amarelo, cinza):
-    print('\n'+"Regras:"+"\n"+'\n'+("- Você tem {0} tentativas para acertar uma palavra aleatória de {1} letras.".format(tentativas,qtde_letras)))
-    print('- A cada tentativas, a palavra testada terá suas suas letras coloridas conforme:')
-    print(' * {0}: a letra está na posição correta;'.format(azul))
-    print(' * {0}: a palavra tem a letra, mas está na posição errada;'.format(amarelo))
-    print(' * {0}: a palavra não tem a letra.'.format(cinza))
-    print('- Os acentos são ignorados;'+'\n'+ '- As palavras podem possuir letras repetidas.' + '\n')
+tentativas = COR_VERMELHA + str(qtde+1) + COR_RESET
+qtde_letras = COR_VERMELHA + str(qtde) + COR_RESET
+azul = COR_AZUL + 'Azul' + COR_RESET
+amarelo = COR_AMARELA + 'Amarelo' + COR_RESET
+cinza = COR_CINZA + 'Cinza' + COR_RESET
+
+print('\n'+"Regras:"+"\n"+'\n'+("- Você tem {0} tentativas para acertar uma palavra aleatória de {1} letras.".format(tentativas,qtde_letras)))
+print('- A cada tentativas, a palavra testada terá suas suas letras coloridas conforme:')
+print(' * {0}: a letra está na posição correta;'.format(azul))
+print(' * {0}: a palavra tem a letra, mas está na posição errada;'.format(amarelo))
+print(' * {0}: a palavra não tem a letra.'.format(cinza))
+print('- Os acentos são ignorados;'+'\n'+ '- As palavras podem possuir letras repetidas.' + '\n')
 
 def print_congratulations(parabens):
     print('\n'+'{0}'.format(parabens))
